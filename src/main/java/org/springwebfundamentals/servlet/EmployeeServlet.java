c7514b841839b68c33950e60e4af046f18f9d384
Unzala <unzala@aeiota.com>
2026-09-22 20:33:39 +0530

    Spring mvc web fundamentals

---
+++ src/main/java/org/springwebfundamentals/servlet/EmployeeServlet.java
@@ -0,0 +1,36 @@
+package org.springwebfundamentals.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/employee/*")
+public class EmployeeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+            // 1. Set the response content type
+            response.setContentType("text/html");
+            // 2. Write a message back to the browser screen
+            PrintWriter out = response.getWriter();
+            String uri = request.getRequestURI();
+            if(uri.endsWith("/details")){
+                out.println("<h1>Employee details</h1>");
+
+            }else{
+                out.println("<h1>Success! Employee Servlet is working perfectly.</h1>");
+                out.println("<h2>HTTP METHOD: "+request.getMethod()+"</h2>");
+                out.println("<h2>Request URI: "+request.getRequestURI()+"</h2>");
+                out.println("<h2>Server Name: "+request.getServerName()+"</h2>");
+                out.println("<h2>Server Port: "+request.getServerPort()+"</h2>");
+            }
+
+    }
+
+
+}
